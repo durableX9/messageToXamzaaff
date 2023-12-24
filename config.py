0abfe4ed47aecc687f820cc65476c730ddab74ae
Rasulov Shodiyor <108322250+ruymhdi@users.noreply.github.com>
2023-12-24 07:32:56 -0500

    updating config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+cfg = 
+{
+	'token': 'token',
+	'name': 'nameOfTheBot',
+	'dev_id': 0, 
+	'sup_chat_id': -0,
+	'db_url': 'link',
+	'db_name': 'dev',
+
+	'button_question': '📩 Savol',
+
+	'welcome_message': "AssalamuAlaykum. xamzaaff telegram kanalining asoschisi bilan bog'lanmoqchi bo'lsangiz, savolingizni qoldirishingizni so'rayman.",
+	'ban_message': "⚠ Siz ushbu botda *BAN* olgansiz!",
+	'question_type_ur_question_message': "Iltimos savolingizni to'g'ri va to'liq bayon qiling." 	
+}
