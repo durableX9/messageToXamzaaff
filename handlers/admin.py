dfc043e55ecf4fd71494818f58fa0cdd111a1e8f
Rasulov Shodiyor <108322250+ruymhdi@users.noreply.github.com>
2023-12-24 11:05:24 -0500

    admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -6,3 +6,16 @@ from keyboards import kb
 from config import cfg
 from handlers.fsm import *
 
+lvl3name = cfg['3lvl_adm_name']
+devid = cfg['dev_id']
+
+def extract_arg(arg):
+	return arg.split()[1:]
+
+async def from_admin(message: types.Message):
+	try: 
+		uid = message.from_user.id
+
+		if(db_profile_access(uid) >= 1):
+			args = extract_arg(message.text)
+		
