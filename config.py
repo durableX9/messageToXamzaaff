df446596fbef549965cf33b003ec93288d26e52e
Rasulov Shodiyor <108322250+ruymhdi@users.noreply.github.com>
2023-12-24 07:40:03 -0500

    updated config.py

--- config.py
+++ config.py
@@ -11,5 +11,8 @@ cfg =
 
 	'welcome_message': "AssalamuAlaykum. xamzaaff telegram kanalining asoschisi bilan bog'lanmoqchi bo'lsangiz, savolingizni qoldirishingizni so'rayman.",
 	'ban_message': "⚠ Siz ushbu botda *BAN* olgansiz!",
-	'question_type_ur_question_message': "Iltimos savolingizni to'g'ri va to'liq bayon qiling." 	
+	'question_type_ur_question_message': "Iltimos savolingizni to'g'ri va to'liq bayon qiling.",
+	'question_ur_sended_message': 'Sizning habaringiz (xamzaaff`ga) yuborildi, iltimos javobimni kuting!',
+
+	'3lvl_adn_name': 'Supervisor'
 }
